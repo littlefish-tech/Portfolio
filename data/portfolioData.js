549a3fab57183e8895d34c3d9cac5c2587888de8
Yimeng Yu <yimeng.yu@gmail.com>
2020-02-07 21:45:08 -0800

    update portfolio api

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -1,34 +1,58 @@
 var portfolioArray = [
     {
-      image: "Jerome",
-      title: "jerome@example.com",
-      description: "yum1234",
-      link: "000-000-0000"
+      image: "img/ThePsychicGame.png",
+      title: "The Psychic Game",
+      description: "The race between you and the computer",
+      link: "https://littlefish-tech.github.io/Psychic-Game/"
     },
      {
-        image: "Jerome",
-        title: "jerome@example.com",
-        description: "yum1234",
-        link: "000-000-0000"
+        image: "img/GifTastic.png",
+        title: "Fun Giphy",
+        description: "Let's explore the magical of Giphy",
+        link: "https://littlefish-tech.github.io/GifTastic/"
     },
      {
-        image: "Jerome",
+        image: "img/trainSchedule.png",
         title: "jerome@example.com",
-        description: "yum1234",
-        link: "000-000-0000"
+        description: "The Train Schedule",
+        link: "https://littlefish-tech.github.io/Train-Scheduler/"
     },
     {
-        image: "Jerome",
-        title: "jerome@example.com",
-        description: "yum1234",
-        link: "000-000-0000"
+        image: "img/crystalGame.png",
+        title: "Click the Crystals",
+        description: "Get the numbers showing on the screen and you will win!.",
+        link: "https://littlefish-tech.github.io/unit-4-game/"
     },
     {
-        image: "Jerome",
-        title: "jerome@example.com",
-        description: "yum1234",
-        link: "000-000-0000"
-    }
+        image: "img/newsBreeze.png",
+        title: "News Breeze",
+        description: "Our first team project. It is the one stop shopping for the top news of the day!!",
+        link: "https://littlefish-tech.github.io/Project-1-Team-04/"
+    },
+    {
+        image: "img/lirinode.png",
+        title: "Liri Node App",
+        description: "I have learned a lot about how node.js works. It is very fun when I can see I get all",
+        link: "https://littlefish-tech.github.io/liri-node-app/"
+    },
+    {
+        image: "img/bamazonlogo.png",
+        title: "bamazon",
+        description: "A cool online shopping and inventory management tool",
+        link: "https://littlefish-tech.github.io/bamazon/"
+    },
+    {
+        image: "img/friendfinderLogo.png",
+        title: "Friend Finder<",
+        description: "A simple friend matching app",
+        link: "https://arcane-ocean-36607.herokuapp.com"
+    },
+    {
+        image: "img/Burgers.png",
+        title: "Love Burgers",
+        description: "My very first full stack app",
+        link: "https://dry-sea-94956.herokuapp.com/"
+    },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
